c2e12ca55c845613158e0114abf5e4c801870e83
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-07-19 12:18:30 +0100

    Minor edits

--- installopenstack.sh
+++ installopenstack.sh
@@ -11,8 +11,8 @@ E_BADARGS=65
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: installopenstack.sh <IP Address> <MySQL Password>"
-  echo "You're prompted for the MySQL Password during install.  The value provided here must be the same."
+  echo "Usage: installopenstack.sh <Public IP Address> <MySQL Password>"
+  echo "You're also prompted for the MySQL Password during install.  The value provided here must be the same."
 exit $E_BADARGS
 fi
 
@@ -47,7 +47,6 @@ figlet Keystone Package -t
 echo "deb http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/folsom main" >> /etc/apt/sources.list.d/folsom.list
 
 sudo apt-get install ubuntu-cloud-keyring -y
-
 apt-get install mysql-server python-mysqldb keystone -y
 
 #Not sure if I should do this, but bombs out otherwise
@@ -80,26 +79,30 @@ ROLE_ID_ADMIN=`mysql -uroot -p$MYSQLPWORD -s -N -e "SELECT id from keystone.role
 
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 user-create --tenant $TENANT_ID_DEFAULT --name admin --pass admin;sleep 0.5
 USER_ID_ADMIN=`mysql -uroot -p$MYSQLPWORD -s -N -e "SELECT id from keystone.user where name='admin'"`;sleep 0.5
-keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 user-role-add --user $USER_ID_ADMIN --tenant $TENANT_ID_DEFAULT --role $ROLE_ID_ADMIN
 
+keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 user-role-add --user $USER_ID_ADMIN --tenant $TENANT_ID_DEFAULT --role $ROLE_ID_ADMIN
 
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 tenant-create --name service --description "Service Tenant";sleep 0.5
 TENANT_ID_SERVICE=`mysql -uroot -p$MYSQLPWORD -s -N -e "SELECT id from keystone.tenant where name='service'"`
 
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 user-create --tenant $TENANT_ID_SERVICE --name glance --pass $GLANCEPASSWORD;sleep 0.5
 USER_ID_GLANCE=`mysql -uroot -p$MYSQLPWORD -s -N -e "SELECT id from keystone.user where name='glance'"`;sleep 0.5
+
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 user-role-add --user $USER_ID_GLANCE --tenant $TENANT_ID_SERVICE --role $ROLE_ID_ADMIN
 
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 user-create --tenant $TENANT_ID_SERVICE --name nova --pass nova;sleep 0.5
 USER_ID_NOVA=`mysql -uroot -p$MYSQLPWORD -s -N -e "SELECT id from keystone.user where name='nova'"`;sleep 0.5
+
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 user-role-add --user $USER_ID_NOVA --tenant $TENANT_ID_SERVICE --role $ROLE_ID_ADMIN
 
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 user-create --tenant $TENANT_ID_SERVICE --name ec2 --pass ec2;sleep 0.5
 USER_ID_EC2=`mysql -uroot -p$MYSQLPWORD -s -N -e "SELECT id from keystone.user where name='ec2'"`;sleep 0.5
+
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 user-role-add --user $USER_ID_EC2 --tenant $TENANT_ID_SERVICE --role $ROLE_ID_ADMIN
 
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 user-create --tenant $TENANT_ID_SERVICE --name swift --pass swift;sleep 0.5
 USER_ID_SWIFT=`mysql -uroot -p$MYSQLPWORD -s -N -e "SELECT id from keystone.user where name='swift'"`;sleep 0.5
+
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 user-role-add --user $USER_ID_SWIFT --tenant $TENANT_ID_SERVICE --role $ROLE_ID_ADMIN
 
 
@@ -108,20 +111,23 @@ echo "Service and Endpoint Configuration"
 #Service and Endpoint Setup
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 service-create --name=keystone --type=identity --description="Keystone Identity Service";sleep 0.5
 SERVICE_ID_KEYSTONE_IDENTITY=`mysql -uroot -p$MYSQLPWORD -s -N -e "SELECT id from keystone.service where type='identity'"`;sleep 0.5
+
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 endpoint-create --region RegionOne --service=$SERVICE_ID_KEYSTONE_IDENTITY --publicurl=http://$KEYSTONE_PUB_IP:5000/v2.0 --internalurl=http://$KEYSTONE_PRIV_IP:5000/v2.0 --adminurl=http://$KEYSTONE_PUB_IP:35357/v2.0
 
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 service-create --name=nova --type=compute --description="Nova Compute Service";sleep 0.5
 SERVICE_ID_NOVA_COMPUTE=`mysql -uroot -p$MYSQLPWORD -s -N -e "SELECT id from keystone.service where type='compute'"`;sleep 0.5
+
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 endpoint-create --region RegionOne --service=$SERVICE_ID_NOVA_COMPUTE --publicurl=http://$NOVA_PUB_IP:8774/v2/%\(tenant_id\)s --internalurl=http://$NOVA_PRIV_IP:8774/v2/%\(tenant_id\)s --adminurl=http://$NOVA_PUB_IP:8774/v2/%\(tenant_id\)s
 
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 service-create --name=cinder --type=volume --description="Cinder Volume Service";sleep 0.5
 SERVICE_ID_CINDER_VOLUME=`mysql -uroot -p$MYSQLPWORD -s -N -e "SELECT id from keystone.service where type='volume'"`;sleep 0.5
+
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 endpoint-create --region RegionOne --service=$SERVICE_ID_CINDER_VOLUME --publicurl=http://$CINDER_PUB_IP:8776/v1/%\(tenant_id\)s --internalurl=http://$CINDER_PRIV_IP:8776/v1/%\(tenant_id\)s --adminurl=http://$CINDER_PUB_IP:8776/v2/%\(tenant_id\)s
 
 keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 service-create --name=glance --type=image --description="Glance Image Service";sleep 0.5
 SERVICE_ID_GLANCE_IMAGE=`mysql -uroot -p$MYSQLPWORD -s -N -e "SELECT id from keystone.service where type='image'"`;sleep 0.5
-keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 endpoint-create --region RegionOne --service=$SERVICE_ID_GLANCE_IMAGE --publicurl=http://$GLANCE_PUB_IP:9292 --internalurl=http://$GLANCE_PRIV_IP:9292 --adminurl=http://$GLANCE_PUB_IP:9292
 
+keystone --token $TOKEN --endpoint http://$KEYSTONE_HOST:35357/v2.0 endpoint-create --region RegionOne --service=$SERVICE_ID_GLANCE_IMAGE --publicurl=http://$GLANCE_PUB_IP:9292 --internalurl=http://$GLANCE_PRIV_IP:9292 --adminurl=http://$GLANCE_PUB_IP:9292
 
 sudo apt-get install python-paste glance glance-client python-mysqldb -y
 
